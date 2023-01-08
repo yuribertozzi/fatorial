7b56220fcaed9688dc5b30cd85e08bcb4a9ddabf
Yuri Silva <121751241+yuribertozzi@users.noreply.github.com>
2023-01-08 16:16:44 -0300

    Add files via upload

---
+++ fatorial_repost_08012023.py
@@ -0,0 +1,22 @@
+n = int(input("Digite o valor de n: "))
+
+
+if n < 0:
+		
+	print("Não existe fatorial de número negativo.")
+
+else:
+
+	fatorial = 1  
+
+	while n > 0:
+	
+		fatorial = fatorial * n
+
+		n = n - 1
+
+	print(fatorial)
+
+	
+	
+	
